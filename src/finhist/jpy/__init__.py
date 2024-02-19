8368f01c0a7858b7500151d1e3025ce62c7ca863
Your Name <you@example.com>
2024-02-19 20:23:04 +0900

    add jpy yield

--- src/finhist/jpy/__init__.py
+++ src/finhist/jpy/__init__.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-def read_m2_jpy():
+def read_jpy_m2():
     # https://www.stat-search.boj.or.jp/ssi/cgi-bin/famecgi2?cgi=$nme_a000&lstSelection=MD02
     dir = os.path.dirname(__file__)
     dfs = [
@@ -9,9 +9,9 @@ def read_m2_jpy():
         pd.read_csv(os.path.join(dir, 'nme_R031.6330.20240219194406.01.csv'), skiprows=1),
         pd.read_csv(os.path.join(dir, 'nme_R031.7259.20240219194913.01.csv'), skiprows=1),
     ]
-    dfs[0] = dfs[0].rename(columns={ 'Ｍ２／平／マネーストック': 'm2_jpy' })
-    dfs[1] = dfs[1].rename(columns={ '（更新停止）Ｍ２＋ＣＤ／平／マネーサプライ（2008年4月まで）': 'm2_jpy' })
-    dfs[2] = dfs[2].rename(columns={ '（更新停止）旧Ｍ２＋ＣＤ／平／マネーサプライ（1999年3月まで）': 'm2_jpy' })
+    dfs[0] = dfs[0].rename(columns={ 'Ｍ２／平／マネーストック': 'jpy_m2' })
+    dfs[1] = dfs[1].rename(columns={ '（更新停止）Ｍ２＋ＣＤ／平／マネーサプライ（2008年4月まで）': 'jpy_m2' })
+    dfs[2] = dfs[2].rename(columns={ '（更新停止）旧Ｍ２＋ＣＤ／平／マネーサプライ（1999年3月まで）': 'jpy_m2' })
 
     dfs2 = []
     for df in dfs:
@@ -20,8 +20,23 @@ def read_m2_jpy():
         dfs2.append(df)
 
     df = pd.concat(dfs2)
-    df = df[['year', 'month', 'm2_jpy']]
+    df = df[['year', 'month', 'jpy_m2']]
     df = df.set_index(['year', 'month']).sort_index()
     df = df.dropna()
     df = df.loc[~df.index.duplicated(keep='last')]
+    return df
+
+
+def read_jpy_yield():
+    # https://www.stat-search.boj.or.jp/ssi/cgi-bin/famecgi2?cgi=$nme_a000&lstSelection=IR01
+    dir = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(dir, 'nme_R031.13609.20240219201551.01.csv'), skiprows=1)
+
+    df = df.rename(columns={ '基準割引率および基準貸付利率': 'jpy_yield' })
+    df['year'] = df['系列名称'].str[:4].astype('int')
+    df['month'] = df['系列名称'].str[5:].astype('int')
+
+    df = df[['year', 'month', 'jpy_yield']]
+    df = df.set_index(['year', 'month']).sort_index()
+    df = df.dropna()
     return df
--- tests/jpy/test_jpy.py
+++ tests/jpy/test_jpy.py
@@ -1,12 +1,17 @@
 from unittest import TestCase
 import numpy as np
 from src.finhist.jpy import (
-    read_m2_jpy
+    read_jpy_m2, read_jpy_yield
 )
 
 
 class TestJpy(TestCase):
-    def test_read_m2sl(self):
-        df = read_m2_jpy()
+    def test_read_jpy_m2(self):
+        df = read_jpy_m2()
         self.assertFalse(np.any(df.index.duplicated()))
-        self.assertEqual(df.loc[(1967, 1), 'm2_jpy'], 282500)
+        self.assertEqual(df.loc[(1967, 1), 'jpy_m2'], 282500)
+
+    def test_read_yield_jpy(self):
+        df = read_jpy_yield()
+        self.assertFalse(np.any(df.index.duplicated()))
+        self.assertEqual(df.loc[(1882, 10), 'jpy_yield'], 10.22)
