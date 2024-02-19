17480080a2d97f817a2b6c6626317f69845be19c
Your Name <you@example.com>
2024-02-19 23:57:52 +0900

    add nikkei225

--- src/finhist/stock/__init__.py
+++ src/finhist/stock/__init__.py
@@ -13,3 +13,16 @@ def read_sp500():
     df = df.drop(columns='date')
     df = df.set_index(['year', 'month'])
     return df
+
+def read_nikkei225():
+    # https://www.macrotrends.net/2593/nikkei-225-index-historical-chart-data
+    dir = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(dir, 'nikkei-225-index-historical-chart-data.csv'), skiprows=15)
+    df.columns = [x.lower() for x in df.columns]
+    df['date'] = pd.to_datetime(df['date'])
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
+    df = df.drop(columns='date')
+    df = df.groupby(['year', 'month']).mean()
+    df = df.rename(columns={ ' value': 'nikkei225' })
+    return df
--- tests/stock/test_stock.py
+++ tests/stock/test_stock.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from src.finhist.stock import read_sp500
+from src.finhist.stock import read_sp500, read_nikkei225
 from ..testing.utils import check_data
 
 
@@ -8,3 +8,9 @@ class TestStock(TestCase):
         df = read_sp500()
         check_data(self, df)
         self.assertEqual(df.loc[(1871, 1), 'sp500'], 4.44)
+
+    def test_read_nikkei225(self):
+        df = read_nikkei225()
+        check_data(self, df)
+        self.assertEqual(df.loc[(1949, 5), 'nikkei225'], 173.01083333333335)
+
